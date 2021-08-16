6f97caad0d88f05f0ff3abac0fbc4d32faba4568
Vigneshraja G <vigneshraja20@gmail.com>
2021-08-16 17:55:42 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function App(): JSX.Element {
-  return <h1>My React and TypeScript App testing success!</h1>;
+  return <h1>My React and TypeScript App testing success</h1>;
 }
